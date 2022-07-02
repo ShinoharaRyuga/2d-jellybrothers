b2bdfae04d6b5a5eff5eb5710ca4cd092acc50cf
SinoharaRyuga <raitan3170@gmail.com>
2022-07-03 01:31:46 +0900

    プレイヤーが同じルームに参加出来るようにした

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,7 +6,8 @@ using Photon.Realtime;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] string _prefabName = "";
+    [SerializeField] string _player1PrefabName = "Player1";
+    [SerializeField] string _player2PrefabName = "Player2";
     [SerializeField] Transform[] _spwanPoint = new Transform[2];
 
     private void Awake()
@@ -19,41 +20,10 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         Connect();
     }
 
-    void Update()
-    {
-       
-    }
-
-    public void Connect()
-    {
-        if (PhotonNetwork.IsConnected == false)
-        {
-            Debug.Log("サーバーに接続");
-            PhotonNetwork.ConnectUsingSettings();
-        }
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("ルームに入室出来ませんでした");
-    }
-
-    public override void OnConnected()
-    {
-        Debug.Log("サーバーに接続成功");
-    }
-
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("ルームを作成");
-    }
-
     public override void OnJoinedLobby()
     {
         Debug.Log("ロビーに参加");
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.IsVisible = true;
-        PhotonNetwork.JoinRandomOrCreateRoom();
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public override void OnConnectedToMaster()
@@ -66,17 +36,53 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("ルームに入室");
+
+        if (PhotonNetwork.LocalPlayer.ActorNumber > PhotonNetwork.CurrentRoom.MaxPlayers - 1)
+        {
+            Debug.Log("人数が集まりました　ルームを閉じます");
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+        }
+
         var index = PhotonNetwork.LocalPlayer.ActorNumber - 1;
-        var player = PhotonNetwork.Instantiate(_prefabName, _spwanPoint[index].position, Quaternion.identity).GetComponent<PlayerController>();
-        
+        PlayerController player = null;
+     
         if (index == 0)
         {
-            player.SetColor(Color.red);
+            player = PhotonNetwork.Instantiate(_player1PrefabName, _spwanPoint[index].position, Quaternion.identity).GetComponent<PlayerController>();
+            player.name = "Player1";
         }
         else if (index == 1)
         {
-            player.SetColor(Color.blue);
+            player = PhotonNetwork.Instantiate(_player2PrefabName, _spwanPoint[index].position, Quaternion.identity).GetComponent<PlayerController>();
+            player.name = "Player2";
         }
-        
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("ルームに入室出来ませんでした ルームを新しく作成します");
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(null, roomOptions);
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("サーバーに接続成功");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("ルームを作成");
+    }
+
+    void Connect()
+    {
+        if (PhotonNetwork.IsConnected == false)
+        {
+            Debug.Log("サーバーに接続");
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,19 @@ using Photon.Pun;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float _speed = 3f;
+    [SerializeField, Tooltip("ˆÚ“®‘¬“x")] float _speed = 3f;
 
     Vector2 _moveDirction = Vector2.zero;
     float _horizontal = 0f;
-    Rigidbody2D _rb2D => GetComponent<Rigidbody2D>();
+    Rigidbody2D _rb2D = default;
     PhotonView _view => GetComponent<PhotonView>();
-    SpriteRenderer _spriteRenderer => GetComponent<SpriteRenderer>();
 
     void Start()
     {
-        
+        if (_view.IsMine)
+        {
+            _rb2D = GetComponent<Rigidbody2D>();
+        }
     }
 
     void Update()
@@ -27,20 +29,16 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-      //  var moveDirction = new Vector2(_horizontal, 0).normalized * _speed;
-
-        if (_moveDirction != Vector2.zero)
+        if (_rb2D != null)
         {
-            _rb2D.velocity = _moveDirction;
+            if (_moveDirction != Vector2.zero)
+            {
+                _rb2D.velocity = _moveDirction;
+            }
+            else
+            {
+                _rb2D.velocity = Vector2.zero;
+            }
         }
-        else
-        {
-            _rb2D.velocity = Vector2.zero;
-        }
-    }
-
-    public void SetColor(Color color)
-    {
-        _spriteRenderer.color = color;
     }
 }
