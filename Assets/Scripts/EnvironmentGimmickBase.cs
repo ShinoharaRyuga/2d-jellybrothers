38d4be70fae31db4b6a3ee3fae72be86af975859
SinoharaRyuga <raitan3170@gmail.com>
2022-07-04 18:42:18 +0900

    コメント追加

--- Assets/Scripts/EnvironmentGimmickBase.cs
+++ Assets/Scripts/EnvironmentGimmickBase.cs
@@ -1,12 +1,16 @@
 using Photon.Pun;
 using UnityEngine;
 
+/// <summary>各ギミックの基底クラス </summary>
 public abstract class EnvironmentGimmickBase : MonoBehaviour
 {
     PhotonView _view => GetComponent<PhotonView>();
 
     public PhotonView View { get => _view; }
 
+    /// <summary>オブジェクトを消す </summary>
     public abstract void SetActiveFalse();
+
+    /// <summary>オブジェクトを出す </summary>
     public abstract void SetActiveTrue();
 }
--- Assets/Scripts/GoalWall.cs
+++ Assets/Scripts/GoalWall.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using UnityEngine;
 
+/// <summary>ゴール直前にある扉　2人同時にボタンを押さなければならない </summary>
 public class GoalWall : EnvironmentGimmickBase
 {
     [PunRPC]
