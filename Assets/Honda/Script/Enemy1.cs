433e6091a9fa996e3ac6407af6611c26862a1724
kazu0221 <kirito-sao.com3290@ezweb.ne.jp>
2022-07-13 17:36:40 +0900

    敵２種類の挙動完成

---
+++ Assets/Honda/Script/Enemy1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : EnemyController
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+    }
+}
