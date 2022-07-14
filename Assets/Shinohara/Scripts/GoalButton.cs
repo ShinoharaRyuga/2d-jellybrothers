59c4ad4915fb041c5e661ae358a24bac934ba18e
SinoharaRyuga <raitan3170@gmail.com>
2022-07-14 15:16:37 +0900

    ギミックの修正　ギミック説明のテキスト追加

--- Assets/Shinohara/Scripts/GoalButton.cs
+++ Assets/Shinohara/Scripts/GoalButton.cs
@@ -4,15 +4,29 @@ using Photon.Pun;
 /// <summary>ゴール直前に存在する扉を開けるためのクラス </summary>
 public class GoalButton : MonoBehaviour
 {
-    [SerializeField, Header("ボタンが押された時に開くドア")] GoalWall _goalGimmick = default;
     [SerializeField, Header("反応するプレイヤー")] Player _player = default;
     [SerializeField, Tooltip("同時に開く為のボタン")] GoalButton _partnerButton = default;
     /// <summary>プレイヤーがボタンに触れているかどうか </summary>
     bool _isHit = false;
+
+    SpriteRenderer _renderer => GetComponent<SpriteRenderer>();
+    SetGoalWall _goalGimmick => transform.parent.GetComponent<SetGoalWall>();
     PhotonView _view => GetComponent<PhotonView>();
     public PhotonView View { get => _view; }
     /// <summary>プレイヤーがボタンに触れているかどうか </summary>
     public bool IsHit { get => _isHit; set => _isHit = value; }
+
+    private void OnValidate()
+    {
+        if (_player == Player.Player1)
+        {
+            _renderer.color = Color.red;
+        }
+        else if(_player == Player.Player2)
+        {
+            _renderer.color = Color.blue;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -23,7 +37,7 @@ public class GoalButton : MonoBehaviour
 
             if (_partnerButton.IsHit && _isHit) //同時にボタンが押されていたら扉を開ける
             {
-                _goalGimmick.View.RPC("SetActiveFalse", RpcTarget.All);
+                _goalGimmick.GoalWall.View.RPC("SetActiveFalse", RpcTarget.All);
             }
         }
     }
