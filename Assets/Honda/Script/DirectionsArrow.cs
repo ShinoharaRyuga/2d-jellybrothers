66e5392b35e135c2793bf14c9ebaf54ef804d11d
kazu0221 <kirito-sao.com3290@ezweb.ne.jp>
2022-10-31 11:37:14 +0900

    道案内用の矢印追加、シーンのオブジェクトを整理

---
+++ Assets/Honda/Script/DirectionsArrow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionsArrow : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] _arrows =  new GameObject[2];//表示する順番にオブジェクトを入れる
+    [SerializeField]Player _player = default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == _player.ToString())
+        {
+            _arrows[0].SetActive(false);
+            _arrows[1].SetActive(true);
+            this.enabled = false;
+        }
+    }
+
+
+    public enum Player
+    { 
+        Player1,
+        Player2
+    }
+
+}
